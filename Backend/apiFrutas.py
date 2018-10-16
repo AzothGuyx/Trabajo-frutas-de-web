9f80831a45f5e732c310c4f85fb4a5caa18e2914
AzothGuyx <cristianxapata@gmail.com>
2018-10-16 18:06:46 -0500

    cambios  de atributos

--- Backend/apiFrutas.py
+++ Backend/apiFrutas.py
@@ -14,35 +14,37 @@ Frutas = [
 
 #Get
 @app.route("/Frutas")
-def allTask():
+def allFrutas():
     return jsonify({'Frutas': Frutas})
 
 #Get One
 @app.route("/Frutas/<int:fruta_id>")
-def getTask(fruta_id):
+def getFruta(fruta_id):
     print(fruta_id)
     return jsonify(Frutas[0]['nombre'])
 
 #post 
 @app.route("/Frutas", methods=['POST'])
-def createTask():
-    #Agregar elemento a un array
+def createFruta():
+    #Agregar elemento a un  array 
+    print(request.json)
     Frutas.append(request.json)
     return jsonify(Frutas)
 
 #PUT One
 @app.route("/Frutas/<int:fruta_id>", methods=['PUT'])
-def putTask(fruta_id):
+def putFruta(fruta_id):
     index=0
     for fruta in Frutas:
         if fruta['id']== fruta_id:
+            print(request.json)
             Frutas[index]=request.json
         index+=1
     return jsonify(Frutas)
 
 #Delete One
 @app.route("/Frutas/<int:fruta_id>", methods=['DELETE'])
-def deleteTask(fruta_id):
+def deleteFruta(fruta_id):
     for fruta in Frutas:
         if fruta['id']== fruta_id:
             Frutas.remove(fruta)
